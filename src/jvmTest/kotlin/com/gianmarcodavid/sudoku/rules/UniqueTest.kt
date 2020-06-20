43f978423248159b1baacf0df37c304cff439ffa
G <gianmarco.david@gmail.com>
2020-06-20 19:46:16 +0200

    Add test for Unique check

---
+++ src/jvmTest/kotlin/com/gianmarcodavid/sudoku/rules/UniqueTest.kt
@@ -0,0 +1,19 @@
+package com.gianmarcodavid.sudoku.rules
+
+import com.gianmarcodavid.sudoku.Coordinate
+import com.gianmarcodavid.sudoku.Fixed
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.checkAll
+
+internal class UniqueTest : StringSpec({
+
+    "true if all unique" {
+        checkAll { ns: List<Int> ->
+            val group = ns.map { Coordinate(0, 0) to Fixed(it) }
+
+            Unique().check(group) shouldBe (ns.size == ns.distinct().size)
+        }
+    }
+
+})
