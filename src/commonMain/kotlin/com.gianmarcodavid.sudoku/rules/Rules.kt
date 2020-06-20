c4f4724a2512cf1229b89a3c93737ce14912279c
G <gianmarco.david@gmail.com>
2020-06-21 01:05:45 +0200

    Split rules, checks, subjects

--- src/commonMain/kotlin/com.gianmarcodavid.sudoku/rules/Rules.kt
+++ src/commonMain/kotlin/com.gianmarcodavid.sudoku/rules/Rules.kt
@@ -1,9 +1,6 @@
 package com.gianmarcodavid.sudoku.rules
 
-import com.gianmarcodavid.sudoku.Check
-import com.gianmarcodavid.sudoku.Coordinate
 import com.gianmarcodavid.sudoku.Rule
-import com.gianmarcodavid.sudoku.Subject
 
 fun sudoku(rows: Int, columns: Int): Rule {
     val side = rows * columns
@@ -16,20 +13,3 @@ fun sudoku(rows: Int, columns: Int): Rule {
         and(InRange(1..(side * side)), Unique())
     )
 }
-
-fun and(vararg operands: Check): Check = And(operands.toList())
-
-fun row(index: Int, size: Int): Subject = slice(size) { Coordinate(index, it) }
-
-fun column(index: Int, size: Int): Subject = slice(size) { Coordinate(it, index) }
-
-fun box(index: Int, rows: Int, columns: Int): Subject {
-    val rstart = index / rows * rows
-    val cstart = index % rows * columns
-    return slice(rows * columns) {
-        Coordinate(rstart + it / columns, cstart + it % columns)
-    }
-}
-
-private fun slice(size: Int, coordinate: (Int) -> Coordinate) =
-    Subject(List(size, coordinate))
